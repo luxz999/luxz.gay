253092553d19a34459b8e5844ef0a0c4f381ac72
luxz999 <104085969+luxz999@users.noreply.github.com>
2022-10-18 18:42:40 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,41 +1,41 @@
-// var message = "Nigger you can't do that";
+var message = "Nigger you can't do that";
 
-// function rtclickcheck(keyp){ 
-//     if (navigator.appName == "Netscape" && keyp.which == 3){
-//         alert(message);
-//         return false; 
-//     }
+function rtclickcheck(keyp){ 
+    if (navigator.appName == "Netscape" && keyp.which == 3){
+        alert(message);
+        return false; 
+    }
 
-//     if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { 
-//         alert(message);
-//         return false; 
-//     } 
-// }
-// document.onkeydown = function(e) {
-// if(event.keyCode == 123) {
-//     alert(message);
-//     return false;
-// }
-// if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//     alert(message);
-//     return false;
-// }
-// if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//     alert(message);
-//     return false;
-// }
-// if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//     alert(message);
-//     return false;
-// }
-// if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//     alert(message);
-//     return false;
-// }
-// if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
-//     alert(message);
-//     return false;
-// }
-// }
+    if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { 
+        alert(message);
+        return false; 
+    } 
+}
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+    alert(message);
+    return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    alert(message);
+    return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+    alert(message);
+    return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    alert(message);
+    return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    alert(message);
+    return false;
+}
+if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+    alert(message);
+    return false;
+}
+}
 
-// document.onmousedown = rtclickcheck;
+document.onmousedown = rtclickcheck;
